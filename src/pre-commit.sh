33042c0739b0bda86f2d455e96ecf5fc5b6c5e4d
Alex Hodson <alexhodson72@gmail.com>
2019-10-01 10:56:04 +0100

    feat(output identifier): identifies a varierty of output strings

---
+++ src/pre-commit.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Redirect output to stderr.
+exec 1>&2
+# enable user input
+exec < /dev/tty
+
+# Test Strings
+# consoleregexp='System.out.print'
+consoleregexp="console."
+# consoleregexp='print'
+
+## Add ". ':(exclude)src/'" for testing purposes
+if test $(git diff --cached | grep ^+ | grep $consoleregexp | wc -l) != 0
+then
+  echo -e "\n\033[1m\e[033mOutputs Found\e[39m\033[0m"
+  git grep -n -E --cached "$consoleregexp" $(git diff --cached --name-only  --diff-filter=d)
+  echo -e "\n"
+  echo "There are some occurrences of '$consoleregexp' at your modification. Please remove them before continuing."
+  echo $yn | grep ^[Yy]$
+  exit 1;
+else
+  echo "No Outputs Found In Staged Files"
+fi
